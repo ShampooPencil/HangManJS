7f709d6175d833cb8dc0e97e24c242b08b12e74d
Your Actual Name <Your Actual Email>
2019-12-10 19:40:03 -0500

    first commit of the repo

--- javascript/main.js
+++ javascript/main.js
@@ -87,5 +87,22 @@ function mainGame() {
 
 }
 mainGame();
+let prom = new Promise((resolve, reject) => {
+    let num = Math.random();
+    if (num < .5) {
+        resolve('Yay!');
+    } else {
+        reject('Ohhh noooo!');
+    }
+});
 
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+};
+
+prom.then(handleSuccess, handleFailure);
 
